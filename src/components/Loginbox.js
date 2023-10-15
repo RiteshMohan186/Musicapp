035cba9e0896b69ed6fb224d04b55a95b6dcf1d0
Ritesh <LCI2021048@iiitl.ac.in>
2023-10-15 13:45:40 +0530

    update

---
+++ src/components/Loginbox.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Loginbox = () => {
+  return (
+    <div className="main">
+        <input type="checkbox" id='chk' aria-hidden='true' />
+
+        <div className="signup">
+          <form>
+            <label htmlFor="chk" aria-hidden='true'>Sign-up</label>
+            <input type="text" name='txt' placeholder='username' required=""/>
+            <input type="email" name='email' placeholder='email' required=""/>
+            <input type="password" name="pswd" placeholder="Password" required=""/>
+            <button>Sign Up</button>
+          </form>
+        </div>
+
+        <div className="login">
+          <form>
+          <label htmlFor="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required=""/>
+					<input type="password" name="pswd" placeholder="Password" required=""/>
+					<button>Login</button>
+          </form>
+        </div>
+      </div>
+  )
+}
+
+export default Loginbox
